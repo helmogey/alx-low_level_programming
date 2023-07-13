d16cf0059b19f15beb538618ea159bb2a9fb8f24
helmogey <h.elmogey@gmail.com>
2023-07-13 20:12:13 +0300

    fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point 
+ * Description: 'print string'
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
