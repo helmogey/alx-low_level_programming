44a47e1fce950c0f5ded5fc2d4e0b3638a709426
helmogey <h.elmogey@gmail.com>
2023-07-13 23:26:31 +0300

    fix betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	printf(stderr, "and that piece of art is useful\" - Dora ");
+	fprintf(stderr, "and that piece of art is useful\" - Dora ");
 	printf("Korpar, 2015-10-19\n");
 	return (0);
 }
