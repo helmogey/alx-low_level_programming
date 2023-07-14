49a1c7b1877faf7c303fdd07eacb612a37b31290
helmogey <h.elmogey@gmail.com>
2023-07-14 19:19:54 +0300

    sec c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i < 10)
+{
+		j = 0;
+		while (j < 10)
+{
+		k = 0;
+		while (k < 10)
+{
+		if (i < j && j < k)
+{
+		putchar('0' + i);
+		putchar('0' + j);
+		putchar('0' + k);
+		if (i < 8 || j < 9)
+{
+		putchar(',');
+		putchar(' ');
+}
+}
+		k++;
+}
+		j++;
+}
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
