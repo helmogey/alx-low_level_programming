72175d45b0ef4ad53fd621d905a3a04c0650e286
helmogey <h.elmogey@gmail.com>
2023-07-14 18:30:05 +0300

    sec c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,16 +9,16 @@ int main(void)
 	int num;
 
 	for (num = 0; num <= 15; num++)
-{
+	{
 	if (num < 10)
-{
-	putchar(num + '0');
-}
+	{
+		putchar(num + '0');
+	}
 	else
-
-	putchar(num - 10 + 'a');
-}
-}
+	{
+		putchar(num - 10 + 'a');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
