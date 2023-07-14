79e59f478743fd1c46171cdd0ecc484958fbcc1e
helmogey <h.elmogey@gmail.com>
2023-07-14 18:23:40 +0300

    sec c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+{
+	if (num < 10)
+{
+	putchar(num + '0');
+}
+	else
+
+	putchar(num - 10 + 'a');
+}
+}
+	putchar('\n');
+	return (0);
+}
