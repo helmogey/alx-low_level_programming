59523db12dc92887aaf1bb1e8e9f542a554d75ba
helmogey <h.elmogey@gmail.com>
2023-07-14 16:37:44 +0300

    sec c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter = 'a';
+	int count = 0;
+
+	while (count < 26)
+		putchar(letter);
+		letter++;
+		count++
+	return (0);
+}
