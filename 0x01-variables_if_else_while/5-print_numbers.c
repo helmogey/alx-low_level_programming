b3f3b59c528541e52072a9e572a2a449f6d37e0a
helmogey <h.elmogey@gmail.com>
2023-07-14 17:06:16 +0300

    sec c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+{
+		printf(count);
+		count++;
+}
+	putchar('\n');
+	return (0);
+}
+
