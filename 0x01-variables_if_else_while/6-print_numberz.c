1f785ec0312a86cf2df7c2e1cc0c8fbe8901e4ed
helmogey <h.elmogey@gmail.com>
2023-07-14 17:35:36 +0300

    sec c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,38 +1,19 @@
 #include <stdio.h>
-/*
- * print_number -  This function prints the digits of a given\
- *  number to the console using putchar.
- * Description: 'print string'
- * @param num The number to print.
- */
-void print_number(int num)
-{
-	if (num < 0)
-{
-	putchar('-');
-	num = -num;
-}
-	if (num / 10)
-{
-	print_number(num / 10);
-}
-	putchar(num % 10 + '0');
-}
 /**
 *main - Entry point
 *Description: 'print string'
 *Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	int count = 0;
 
 	while (count < 10)
 {
-		print_number(count);
+		putchar('0' + count);
 		count++;
 }
+	putchar('\n');
 	return (0);
 }
 
