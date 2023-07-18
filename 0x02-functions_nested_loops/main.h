b8b62c08d6459f408e2c4015f9ddf3f991f1d844
helmogey <h.elmogey@gmail.com>
2023-07-18 13:28:00 +0300

    1st

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -20,3 +20,25 @@ c++;
 _putchar('\n');
 return;
 }
+
+/**
+*print_alphabet_x10 - helper function
+*Description: 'print string'
+*@c: charecter to start
+*@i: counter
+*Return: Always 0 (Success)
+*/
+int _putchar(char c);
+
+void print_alphabet_x10(void)
+{
+char c = 'a';
+int i = 0;
+for (i = 0; i < 10, ++i)
+{
+print_alphabet()
+}
+_putchar('\n');
+return;
+}
+
