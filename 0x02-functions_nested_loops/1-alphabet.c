183203b64fce9fee31bbeb521169e92fe0087d66
helmogey <h.elmogey@gmail.com>
2023-07-18 13:21:43 +0300

    1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,8 @@
 /**
-*main - Entry point
+*main - entry point
 *Description: 'print string'
+*@c: charecter to start
+*@i: counter
 *Return: Always 0 (Success)
 */
-void print_alphabet(void);
-
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+int main(void);
