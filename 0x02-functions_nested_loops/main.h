ca5b41efb7e236b570a03490d6c9cec852c9910c
helmogey <h.elmogey@gmail.com>
2023-07-18 13:33:52 +0300

    1st

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -41,3 +41,25 @@ print_alphabet();
 return;
 }
 
+/**
+*_islower - helper function
+*Description: 'print string'
+*@c: input charecter to be checked
+*@out: output to be saved
+*Return: Always 0 (Success)
+*/
+int _islower(char c)
+{
+int out;
+
+if (islower(c))
+{
+	out = 1;
+}
+else
+{
+	out = 0;
+}
+return (out);
+}
+
