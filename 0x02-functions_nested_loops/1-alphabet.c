a48f3c682d984cb846749eb510f0678b3b831078
helmogey <h.elmogey@gmail.com>
2023-07-18 12:31:24 +0300

    1st

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,10 @@
-#include <main.h>
-/**
-*print_alphabet - helper function
-*Description: 'print string'
-*Return: Always 0 (Success)
-*/
-void print_alphabet(void)
-{
-char alpha = 'abcdefghijklmnopqrstuvwxyz';
-
-_putchar(alpha);
-_putchar('\n');
-return (0);
-}
 /**
 *main - Entry point
 *Description: 'print string'
 *Return: Always 0 (Success)
 */
+void print_alphabet(void);
+
 int main(void)
 {
 print_alphabet();
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+/**
+*print_alphabet - helper function
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+_putchar(alpha);
+_putchar('\n');
+return (0);
+}
+
