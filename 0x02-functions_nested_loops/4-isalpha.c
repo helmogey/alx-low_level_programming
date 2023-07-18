ea32bafd3f87aec2ed6b40ea010a8ab73e40d78b
helmogey <h.elmogey@gmail.com>
2023-07-18 13:36:34 +0300

    1st

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,42 +1,6 @@
-#include <main.h>
-/**
-*_isalpha - helper function
-*Description: 'print string'
-*@c: input charecter to be checked
-*@out: output to be saved
-*Return: Always 0 (Success)
-*/
-int _isalpha(char c)
-{
-int out;
-
-if (isalpha(c))
-{
-	out = 1;
-}
-else
-{
-	out = 0;
-}
-return (out);
-}
 /**
 *main - Entry point
 *Description: 'print string'
 *Return: Always 0 (Success)
 */
-int main(void)
-{
-int r;
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
-}
+int main(void);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -64,3 +64,26 @@ else
 return (out);
 }
 
+
+/**
+*_isalpha - helper function
+*Description: 'print string'
+*@c: input charecter to be checked
+*@out: output to be saved
+*Return: Always 0 (Success)
+*/
+int _isalpha(char c)
+{
+int out;
+
+if (isalpha(c))
+{
+	out = 1;
+}
+else
+{
+	out = 0;
+}
+return (out);
+}
+
