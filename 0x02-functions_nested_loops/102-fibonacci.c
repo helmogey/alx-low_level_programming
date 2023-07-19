451a969e56254bc67b210517f6b077dce0cce457
helmogey <h.elmogey@gmail.com>
2023-07-19 08:33:54 +0300

    1st

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,21 +2,22 @@
 /**
 *main - Entry point
 *Description: 'print string'
-*@a: integer
-*@b: integer
-*@c: integer
-*@i: integer
+*@a : integer to be the final
+*@b : integer
+*@c : integer
+*@i : integer
 *Return: Always 0 (Success)
 */
-int main() {
-    long int a = 1, b = 2, c, i;
+int main(void)
+{
+long int a = 1, b = 2, c, i;
 
-    for (i = 1; i <= 50; ++i)
-    {
-        printf("%ld\n", a);
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    return 0;
+for (i = 1; i <= 50; ++i)
+{
+printf("%ld, ", a);
+c = a + b;
+a = b;
+b = c;
+}
+return (0);
 }
