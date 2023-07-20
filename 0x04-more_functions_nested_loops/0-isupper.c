7401472b8f7183fcc9666629000559f3af107459
helmogey <h.elmogey@gmail.com>
2023-07-20 10:52:43 +0300

    4th

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/**
+*_isupper - helper function
+*Description: 'print string'
+*@c: input charecter
+*Return: Always 0 (Success)
+*/
+int _isupper(int c)
+{
+int out;
+
+if (isupper(c))
+{
+out = 1;
+}
+else
+{
+out = 0;
+}
+return (out);
+}
