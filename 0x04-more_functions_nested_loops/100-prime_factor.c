7f8f266480032c7a565d1380da292a3f93ef1ad7
helmogey <h.elmogey@gmail.com>
2023-07-20 16:50:45 +0300

    adv

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -24,3 +24,43 @@ factor++;
 printf("%ld", largest_factor);
 return (0);
 }
+
+
+
+
+void print_number(int n)
+{
+if (n < 10)
+{
+_putchar('0' + n);    
+}
+else if (n < 100)
+{
+_putchar('0' + n / 10);
+_putchar('0' + n / 10);
+}
+else if (n < 1000)
+{
+_putchar('0' + n / 100);
+_putchar('0' + n / 100);
+}
+else if (n < 10000)
+{
+_putchar('0' + n / 1000);
+_putchar('0' + n / 1000);
+}
+else if (n < 10000)
+{
+_putchar('0' + n / 1000);
+_putchar('0' + n / 1000);
+}
+else if (n < 10000)
+{
+_putchar('0' + n / 1000);
+_putchar('0' + n / 1000);
+}
+}
+
+
+
+
