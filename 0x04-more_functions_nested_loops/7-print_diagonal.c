0524d95dd1280ecb978b74f509da7da30e9863c3
helmogey <h.elmogey@gmail.com>
2023-07-20 15:23:40 +0300

    fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,14 +18,14 @@ int k;
 
 if (n > 0)
 {
-for (j = 0; j <= n; ++j)
+for (j = 0; j < n; ++j)
 {
 for (k = 0; k < j; ++k)
 {
 _putchar(' ');
 }
 _putchar('\\');
-_putchar(' ');
+_putchar('\n');
 }
 }
 _putchar('\n');
