33a87af9105cd8feab74530dab3c83e76715c716
helmogey <h.elmogey@gmail.com>
2023-07-20 16:16:55 +0300

    fix3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,27 +1,6 @@
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c);
-/**
-*print_most_numbers - helper function
+*main - Entry point
 *Description: 'print string'
 *Return: Always 0 (Success)
 */
-void print_most_numbers(void)
-{
-int i;
-
-for (i = 0; i <= 9; ++i)
-{
-if (i != 2 && i != 4)
-{
-_putchar('0' + i);
-}
-}
-_putchar('\n');
-return;
-}
-
+int main(void);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -29,3 +29,23 @@ _putchar('\n');
 return;
 }
 
+/**
+*print_most_numbers - helper function
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; ++i)
+{
+if (i != 2 && i != 4)
+{
+_putchar('0' + i);
+}
+}
+_putchar('\n');
+return;
+}
+
