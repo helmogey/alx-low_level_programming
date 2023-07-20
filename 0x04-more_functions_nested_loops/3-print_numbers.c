85154cea7b5ce787278a1c9157e8a9d9d25954f6
helmogey <h.elmogey@gmail.com>
2023-07-20 16:15:18 +0300

    fix3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,25 +1,6 @@
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c);
-
-/**
-*print_numbers - helper function
+*main - Entry point
 *Description: 'print string'
 *Return: Always 0 (Success)
 */
-
-void print_numbers(void)
-{
-int i;
-
-for (i = 0; i <= 9; ++i)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
-return;
-}
+int main(void);
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,24 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char c);
+
+
+/**
+*print_numbers - helper function
+*Description: 'print string'
+*Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; ++i)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+return;
+}
+
