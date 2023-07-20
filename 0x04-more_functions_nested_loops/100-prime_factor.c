f4c8682f83864ea5713dfb9532c3f89319ee3a43
helmogey <h.elmogey@gmail.com>
2023-07-20 16:43:49 +0300

    adv

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+* Return: Always 0.
+*/
 int main(void)
 {
 long number = 612852475143;
-long factor = 2L;
-long largest_factor = 0L;
+long factor = 2;
+long largest_factor = 0;
 
 while (number > 1)
 {
