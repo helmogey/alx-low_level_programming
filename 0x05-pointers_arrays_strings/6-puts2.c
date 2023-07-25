c4440f61c3b66206f227547c8e4324d910a83818
helmogey <h.elmogey@gmail.com>
2023-07-25 13:13:35 +0300

    5th

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include<string.h>
+/**
+ * rev_string - check the code
+ * @str: input string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+int l = strlen(str);
+int i;
+
+for(i = 0; i < l; i+=2)
+{
+_putchar(str[i]);
+}
+return;
+}
