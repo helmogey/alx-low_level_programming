bb7962d2def8106d46a9bb9305cc9f865e04ae0b
helmogey <h.elmogey@gmail.com>
2023-07-25 14:26:23 +0300

    5th

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * rev_string - check the code
+ * print_array - check the code
  * @a:input array
  * @n:number of elements of the array to be printed
  * Return: Always 0.
@@ -9,10 +9,17 @@ void print_array(int *a, int n)
 {
 int i;
 
+if (n > 0)
+{
 for (i = 0; i < n - 1; ++i)
 {
 printf("%d, ", a[i]);
 }
 printf("%d\n", a[i]);
+}
+else
+{
+printf("\n");
+}
 return;
 }
