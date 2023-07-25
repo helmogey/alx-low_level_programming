e843b7443c220e9700319a24bd6f6678a34df9eb
helmogey <h.elmogey@gmail.com>
2023-07-25 14:20:20 +0300

    5th

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include<string.h>
+/**
+ * rev_string - check the code
+ * @a:input array
+ * @n:number of elements of the array to be printed
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int 1;
+
+for (i = 0, i < n; ++i)
+{
+printf("%d, ", a[i]);
+}
+printf("\n");
+return;
+}
