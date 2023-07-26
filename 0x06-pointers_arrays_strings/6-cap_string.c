1788e011ceb663a855f93d035b98de825e5cfae1
helmogey <h.elmogey@gmail.com>
2023-07-26 22:45:21 +0300

    6th

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ int i;
 
 for (i = 0; str[i] != '\0'; ++i)
 {
-if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n')
+if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\n' || str[i - 1] == '.')
 {
 str[i] = toupper(str[i]);
 }
