53756ca7c1a297fc26c51ce86c748d36fde8d0cc
helmogey <h.elmogey@gmail.com>
2023-07-27 08:23:33 +0300

    fix 6th

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,29 +6,18 @@
  */
 char *leet(char *str)
 {
-int i;
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
 
-for (i = 0; i < (int)strlen(str) - 1; ++i)
+for (i = 0; str[i] != '\0'; i++)
 {
-if (str[i] == 'A' || str[i] == 'a')
+for (j = 0; j < 10; j++)
 {
-str[i] = '4';
+if (str[i] == a[j])
+{
+str[i] = b[j];
 }
-else if (str[i] == 'E' || str[i] == 'e')
-{
-str[i] = '3';
-}
-else if (str[i] == 'O' || str[i] == 'o')
-{
-str[i] = '0';
-}
-else if (str[i] == 'T' || str[i] == 't')
-{
-str[i] = '7';
-}
-else if (str[i] == 'L' || str[i] == 'l')
-{
-str[i] = '1';
 }
 }
 return (str);
