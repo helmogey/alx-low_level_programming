ec7b671a375b7689bd150b67e4e4a55bcdfcfd1b
helmogey <h.elmogey@gmail.com>
2023-08-02 13:59:54 +0300

    8th

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include <string.h>
+/**
+ * helper_fn - check the code
+ * @s: input string
+ * @l: lenght
+ 8 @n: number
+ * Return: charecter.
+ */
+int helper_fn(char s, int l, int n)
+{
+if (n == l)
+{
+return (1);
+}
+else if (*(s + l - n) != *(s + l + n))
+{
+return (0);
+}
+else
+{
+return (helper_fn(s, l, n + 1));
+}
+}
+/**
+ * is_palindrome - check the code
+ * @s: input string
+ * Return: charecter.
+ */
+int is_palindrome(char *s)
+{
+if (strlen(s) % 2 == 0)
+{
+return (0);
+}
+else
+{
+return (helper_fn(s, (strlen(s) / 2) + 1, 0));
+}
+}
