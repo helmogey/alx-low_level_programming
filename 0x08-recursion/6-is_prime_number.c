84f36a6099b03f85763b6f6d8bf27d997abbb78e
helmogey <h.elmogey@gmail.com>
2023-08-02 13:40:51 +0300

    8th

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,30 @@
 /**
- * is_prime_number - check the code
+ * helper_fn - check the code
+ * @n: input number
+ * @i: number
+ * Return: charecter.
+ */
+int helper_fn(int n, int i)
+{
+if (i == 1)
+return (1);
+if (n % i == 0 && i > 0)
+return (0);
+return (helper_fn(n, i - 1));
+}
+/**
+ * _sqrt_recursion - check the code
  * @n: input number
  * Return: charecter.
  */
-int is_prime_number(int n)
+int _sqrt_recursion(int n)
 {
-if (n <= 1)
+if (n < 0)
 {
-return (0);
+return (-1);
 }
 else
 {
-return (n / is_prime_number(n - 1));
+return (helper_fn(n, n - 1));
 }
 }
