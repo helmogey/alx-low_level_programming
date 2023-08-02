b3e34c84ba88dc1b62381244d00e1c53e138e126
helmogey <h.elmogey@gmail.com>
2023-08-02 12:38:30 +0300

    8th

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+/**
+ * factorial - check the code
+ * @n: input number
+ * Return: charecter.
+ */
+int factorial(int n)
+{
+int i;
+int f = 0;
+
+if (n > 0)
+{
+f = f + n;
+n--;
+}
+return (f);
+}
