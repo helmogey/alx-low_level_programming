18ac949ef1502605115ddfa6e1e592eb1671c544
helmogey <h.elmogey@gmail.com>
2023-08-04 19:02:17 +0300

    10th

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-*_isupper - helper function
+*main - start
 *Description: 'print string'
-*@c: input charecter
+*@argc: number of arguments
+*@argv: arguments
 *Return: Always 0 (Success)
 */
 
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", *argv);
 return (0);
