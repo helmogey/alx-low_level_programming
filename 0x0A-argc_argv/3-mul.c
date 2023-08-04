3703ae7deb4f3a7101655e14af444f8771acf19a
helmogey <h.elmogey@gmail.com>
2023-08-04 21:40:04 +0300

    10th

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,6 @@
 *@argv: arguments
 *Return: Always 0 (Success)
 */
-
 int main(int argc, char *argv[])
 {
 int n1;
@@ -20,6 +19,6 @@ return (1);
 }
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
-printf("%d\n", n1 * n2);
+printf("%d\n", n1 *n2);
 return (0);
 }
