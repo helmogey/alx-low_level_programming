620bf0fb86aceeb90146499ccb6724fcba1530e9
helmogey <h.elmogey@gmail.com>
2023-08-04 18:36:35 +0300

    10th

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*_isupper - helper function
+*Description: 'print string'
+*@c: input charecter
+*Return: Always 0 (Success)
+*/
+
+void main (char *argv[])
+{
+printf("%s\n", *argv);
+return;
+}
