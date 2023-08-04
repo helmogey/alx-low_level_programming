6c572e418007e9394c5d8396b6437b63bb8689c5
helmogey <h.elmogey@gmail.com>
2023-08-04 19:04:34 +0300

    10th

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - start
+*Description: 'print string'
+*@argc: number of arguments
+*@argv: arguments
+*Return: Always 0 (Success)
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%d\n", *argv);
+return (0);
+}
+
