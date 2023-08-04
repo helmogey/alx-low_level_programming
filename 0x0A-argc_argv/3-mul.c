d058cbc749aa10d32e0e53d351ef1f3d4d28d1f1
helmogey <h.elmogey@gmail.com>
2023-08-04 21:34:57 +0300

    10th

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
 *main - start
 *Description: 'print string'
@@ -10,10 +10,16 @@
 
 int main(int argc, char *argv[])
 {
+int n1;
+int n2;
+
 if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
-printf("%d\n", argv[1] * argv[2]);
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 * n2);
+return (0);
 }
