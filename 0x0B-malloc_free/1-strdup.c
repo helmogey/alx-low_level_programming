03afaa08a6abd030f358a586a1c1a352634e104e
helmogey <h.elmogey@gmail.com>
2023-08-07 23:05:24 +0300

    11th

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+*strdup - start
+*Description: 'print string'
+*@str: size
+*Return: Always 0 (Success)
+*/
+char *_strdup(char *str)
+{
+char *str2 = (char *) malloc(sizeof(str));
+unsigned int i = 0;
+
+if (str == NULL)
+{
+return (NULL);
+}
+while (i < sizeof(str))
+{
+str2[i] = str[i];
+i++;
+}
+return (str);
+}
