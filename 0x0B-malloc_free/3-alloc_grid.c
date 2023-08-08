6948720d7956166056e74941308045b747a444ff
helmogey <h.elmogey@gmail.com>
2023-08-08 20:46:34 +0300

    11th

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+*alloc_grid - start
+*Description: 'print string'
+*@width: first string
+*@height: second string
+*Return: Always 0 (Success)
+*/
+int **alloc_grid(int width, int height)
+{
+int *res = malloc(sizeof(int) * width * height);
+int i;
+
+if (res == NULL)
+{
+return (NULL);
+}
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+for (i = 0; i < width * height; i++)
+{
+res[i] = 0;
+}
+return (res);
+}
