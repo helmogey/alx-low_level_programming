4854ddf267eaeb8397f076f28f0d510dc65bafb1
helmogey <h.elmogey@gmail.com>
2023-08-10 23:41:10 +0300

    c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+*_calloc - start
+*Description: 'print string'
+*@nmemb: first
+*@size: second
+*Return: Always 0 (Success)
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *n = malloc(nmemb * size);
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+if (n == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < nmemb * size; i++)
+{
+n[i] = "0";
+}
+return (n);
+}
