c0a4cce8f60b31858b608b0887d062b24ec0f60d
helmogey <h.elmogey@gmail.com>
2023-08-11 00:38:08 +0300

    c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,24 +9,43 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+char *ptr1;
+char *old_ptr;
 unsigned int i;
-void *p;
 
 if (new_size == old_size)
 {
 return (ptr);
 }
-if (new_size == 0 && ptr != NULL)
+if (new_size == 0 && ptr)
 {
 free(ptr);
+return (NULL);
+}
+if (!ptr)
+{
+return (malloc(new_size));
+}
+ptr1 = malloc(new_size);
+if (!ptr1)
+{
+return (NULL);
+}
+old_ptr = ptr;
+if (new_size < old_size)
+{
+for (i = 0; i < new_size; i++)
+{
+ptr1[i] = old_ptr[i];
+}
 }
 if (new_size > old_size)
 {
-p = malloc(new_size);
 for (i = 0; i < old_size; i++)
 {
-p[i] = ptr[i];
+ptr1[i] = old_ptr[i];
 }
-return (p);
 }
+free(ptr);
+return (ptr1);
 }
