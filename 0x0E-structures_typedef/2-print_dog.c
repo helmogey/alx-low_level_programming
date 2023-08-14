2524cb2d90a5761fb491fa377a8762c83451fa34
helmogey <h.elmogey@gmail.com>
2023-08-14 13:49:48 +0300

    eth

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+*print_dog - start
+*Description: 'print string'
+*@d: input
+*Return: void
+*/
+void print_dog(struct dog *d);
+{
+printf("Name: %s\nAge: %d\nOwner: %s\n", dog.name, dog.age, dog.owner);
+return;
+}
