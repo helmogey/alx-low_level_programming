eb3e436675644462bac7466f345152260deb44fa
helmogey <h.elmogey@gmail.com>
2023-08-14 14:26:40 +0300

    eth

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+*free_dog - start
+*Description: 'print string'
+*@d: first
+*Return: void
+*/
+void free_dog(dog_t *d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+return;
+}
