d88204fb71953fd729dfd21b018db12941545f99
helmogey <h.elmogey@gmail.com>
2023-08-14 13:19:31 +0300

    eth

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - a dog
+* @name: First member
+* @age: Second member
+* @owner: Third member
+* Description: Longer description
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
+#endif
