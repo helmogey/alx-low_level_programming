e09d0a69f1c5f098ac2f51f02332285cdd906b1a
helmogey <h.elmogey@gmail.com>
2023-08-16 21:39:26 +0300

    fth

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -16,7 +16,7 @@ return (a + b);
 *@b: int
 *Return: void
 */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
 return (a - b);
 }
@@ -27,7 +27,7 @@ return (a - b);
 *@b: int
 *Return: void
 */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 return (a * b);
 }
@@ -38,7 +38,7 @@ return (a * b);
 *@b: int
 *Return: void
 */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
 return (a / b);
 }
@@ -49,7 +49,7 @@ return (a / b);
 *@b: int
 *Return: void
 */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
 return (a % b);
 }
