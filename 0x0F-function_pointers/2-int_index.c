8a0b322f5fb1156a17d151ac7b1b4d81ea0ff8f0
helmogey <h.elmogey@gmail.com>
2023-08-16 13:14:19 +0300

    eth

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+/**
+*int_index - start
+*Description: 'print string'
+*@array: name
+*@size: size
+*@cmp: function
+*Return: void
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+unsigned int i;
+int out;
+
+if (size <= 0)
+{
+return (-1);
+}
+if (!array || !cmp)
+{
+return;
+}
+for (i = 0; i < size; i++)
+{
+out = cpm(array[i]);
+if (out > 0)
+{
+return (i);
+}
+}
+return (-1);
+}
+
