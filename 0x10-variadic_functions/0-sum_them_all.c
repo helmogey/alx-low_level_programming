c63eb45c437f4c4cdf98fc7707ba6507998a3abc
helmogey <h.elmogey@gmail.com>
2023-08-17 12:06:04 +0300

    10th

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+/**
+*sum_them_all - start
+*Description: 'print string'
+*@n: name
+*Return: void
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int res = 0;
+va_list data;
+
+va_start(data, n);
+for (i = 0; i < n; i++)
+{
+res = res + va_arg(data, int);
+}
+va_end(data);
+return (res);
+}
