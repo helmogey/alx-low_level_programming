b7d5065d22190488ca294a43c3026774e3f43399
helmogey <h.elmogey@gmail.com>
2023-08-17 11:51:03 +0300

    fth

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int data, i;
+char *arr;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(98);
+}
+data = atoi(argv[1]);
+if (data < 0)
+{
+printf("Error\n");
+exit(99);
+}
+arr = (char *)main;
+for (i = 0; i < data; i++)
+{
+if (i == bytes - 1)
+{
+printf("%02hhx\n", arr[i]);
+break;
+}
+printf("%02hhx ", arr[i]);
+}
+return (0);
+}
