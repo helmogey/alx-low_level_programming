968086c5e7578aca7c0ff406f05edb3d961a79cf
helmogey <h.elmogey@gmail.com>
2023-08-25 01:12:44 +0300

    12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*print_list - start
+*Description: 'print string'
+*@h: sep
+*Return: void
+*/
+size_t print_list(const list_t *h)
+{
+size_t num = 0;
+
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+num++;
+h = h->next'
+}
+return (num);
+}
