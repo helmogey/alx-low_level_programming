9dadb9c3a469b8da128219526dab11089899d7f1
helmogey <h.elmogey@gmail.com>
2023-08-25 01:44:47 +0300

    12

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+*add_node - start
+*Description: 'print string'
+*@head: sep
+*@str: sec
+*Return: void
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *first;
+unsigned int len = 0;
+
+while (str[len])
+{
+len++;
+}
+first = malloc(sizeof(list_t));
+if (first == NULL)
+{
+return (NULL);
+}
+first->str = strdup(str);
+first->len = len;
+first->next = *head
+*head = first;
+return (*head);
+}
