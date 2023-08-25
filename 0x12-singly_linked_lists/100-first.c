2d775089e6b023cc7fa730900750923b3cbfd9f3
helmogey <h.elmogey@gmail.com>
2023-08-25 12:19:03 +0300

    12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+void first(void) attribute ((constructor));
+/**
+*print_first - start
+*Description: 'print string'
+*Return: void
+*/
+void print_first(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+return;
+}
