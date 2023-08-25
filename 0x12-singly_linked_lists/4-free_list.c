333194ebe7cf9df26a8baa08c82a720fdf3ee993
helmogey <h.elmogey@gmail.com>
2023-08-25 12:08:40 +0300

    12

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*free_list - start
+*Description: 'print string'
+*@head: sep
+*Return: void
+*/
+void free_list(list_t *head)
+{
+list_t *current = *head;
+
+while (head != NULL)
+{
+current = head->next;
+free(head);
+head = current;
+}
+return;
+}
