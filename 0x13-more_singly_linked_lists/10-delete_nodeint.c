b50a0dd86f1d85459a2264681373bd6d47b9d4fa
helmogey <h.elmogey@gmail.com>
2023-08-28 22:53:12 +0300

    13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 listint_t *current = *head;
 unsigned int len = 0;
 
-if (*head == NULL || tmp == NULL)
+if (*head == NULL)
 {
 return (-1);
 }
