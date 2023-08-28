66951755f3f813a6a0f4b2efd49d8c3c370331b8
helmogey <h.elmogey@gmail.com>
2023-08-28 11:38:24 +0300

    13th

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_listint - start
+*Description: 'print string'
+*@head: sep
+*Return: void
+*/
+void free_listint(listint_t *head)
+{
+listint_t *current;
+while (head->next)
+{
+current = head->next;
+free(head);
+head = current;
+}
+return;
+}
