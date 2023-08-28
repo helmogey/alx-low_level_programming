5107afc09c8be4a1e0d77fde31bf62e55ac8afc9
helmogey <h.elmogey@gmail.com>
2023-08-28 21:20:19 +0300

    13

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 listint_t *current;
 
-while (head)
+while (*head)
 {
-current = head->next;
-free(head);
-head = current;
+current = *head->next;
+free(*head);
+*head = current;
 }
-head = NULL;
+*head = NULL;
 return;
 }
