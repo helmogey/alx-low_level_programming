94ae99351ce7508a5728b8db03749d9da396d9d0
helmogey <h.elmogey@gmail.com>
2023-08-28 10:30:12 +0300

    13th

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*print_listint - start
+*Description: 'print string'
+*@h: sep
+*Return: void
+*/
+size_t print_listint(const listint_t *h)
+{
+size_t num = 0;
+
+while (h)
+{
+printf("%d", h->n);
+num++;
+h = h->next;
+}
+return (num);
+}
