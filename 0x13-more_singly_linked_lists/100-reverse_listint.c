4ce3b9b1c36690ca8090a945095a396dfd2e397c
helmogey <h.elmogey@gmail.com>
2023-08-29 14:46:43 +0300

    13

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+*reverse_listint - start
+*Description: 'print string'
+*@head: sep
+*Return: void
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *previous = NULL;
+listint_t *current = (*head)->next;
+listint_t *nextNode = NULL;
+
+while (current != NULL)
+{
+nextNode = current->next;
+current->next = previous;
+previous = current;
+current = nextNode;
+}
+(*head)->next = previous;
+}
