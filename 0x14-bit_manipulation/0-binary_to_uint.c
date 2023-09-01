ef235a0b3075fc889becfa322e4d44a93591402d
helmogey <h.elmogey@gmail.com>
2023-09-01 16:08:26 +0300

    14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
 *binary_to_uint - start
 *Description: 'print string'
@@ -6,21 +7,25 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int i, res = 0, len = 0;
+unsigned int res = 0, len = 0;
 
 if (b == NULL)
 {
 return (0);
 }
-while (b[len])
+while (b[len] != '\0')
 {
-if (b[len] != '0' && b[len] != '1')
+if (b[len] == '0')
+{
+res = (res <<1) | 0;
+}
+else if (b[len] == '1')
+{
+res = (res << 1) | 1;
+}
+else
 {
 return (0);
-}
-if (b[len] == '1')
-{
-res = res + (2 * len);
 }
 len++;
 }
