6eb15eb161a60d4ec9cf1e4e12fcf3bb4e6773cd
helmogey <h.elmogey@gmail.com>
2023-09-05 00:09:02 +0300

    15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,29 @@
 int create_file(const char *filename, char *text_content)
 {
 size_t byte_written;
-int file descriptor;
+int file_descriptor;
 
 if (text_content == NULL)
 {
 return (-1);
 }
-file_descriptor = open(filename, OCREAT | OTRUNC, 0600);
+file_descriptor = open(filename, O_CREAT | O_TRUNC, 0600);
 if (file_descriptor == -1)
 {
 return (-1);
 }
 close(file_descriptor);
-file_descriptor = open(filename, O_CREAT | OWRONLY, 0600);
+file_descriptor = open(filename, O_CREAT | O_WRONLY, 0600);
 if (file_descriptor == -1)
 {
 return (-1);
 }
-if (byte_written == -1)
+byte_written = write(file_descriptor, text_content, strlen(text_content));
+if (byte_written == (unsigned int)-1)
 {
 close(file_descriptor);
 return (-1);
 }
-close(file_dexcriptor)
+close(file_descriptor);
 return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 
 
 
