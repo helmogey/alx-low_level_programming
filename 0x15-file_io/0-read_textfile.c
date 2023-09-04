b91e1c4c83918c3b29109301fec48b8020934f88
helmogey <h.elmogey@gmail.com>
2023-09-04 23:41:16 +0300

    15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,20 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+FILE *file;
 size_t bytes_read;
-char buffer[letters];
+char *buffer = malloc(letters);
 
-if (filename == NULL)
+if (filename == NULL || buffer == NULL)
 {
 return (0);
 }
-FILE *file = fopen(filename, "r");
+file = fopen(filename, "r");
 if (file == NULL)
 {
 return (0);
 }
-vytes_read = fread(buffer, 1, letters, file);
+bytes_read = fread(buffer, 1, letters, file);
 fclose(file);
 write(STDOUT_FILENO, buffer, bytes_read);
 return (bytes_read);
