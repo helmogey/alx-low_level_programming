773186d75a0c24e327a85e7c556f0d6bdb186a30
helmogey <h.elmogey@gmail.com>
2023-10-03 08:21:07 +0300

    fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef FILR_IO
+#define FILR_IO
+
 #include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -10,5 +13,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#endif
 
 
