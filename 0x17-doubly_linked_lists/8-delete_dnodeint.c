b81be4e80f9f73c381cfa3ad3d9b483ffdbc8074
helmogey <h.elmogey@gmail.com>
2024-01-11 14:12:01 +0200

    c

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ *delete_dnodeint_at_index - doubly linked list
+ *@head: input
+ *@index: input
+ *Description: doubly linked list node structure
+ *Return: int
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *current, *temp;
+
+if (head == NULL || *head == NULL)
+{
+return (-1);
+}
+if (index == 0)
+{
+temp = *head;
+*head = (*head)->next;
+if (*head != NULL)
+{
+(*head)->prev = NULL;
+}
+free(temp);
+return 1;
+}
+current = *head;
+for (unsigned int i = 0; i < index; i++)
+{
+if (current == NULL)
+{
+return (-1);
+}
+current = current->next;
+}
+if (current == NULL)
+{
+return -1;
+}
+if (current->prev != NULL)
+{
+current->prev->next = current->next;
+}
+if (current->next != NULL)
+{
+current->next->prev = current->prev;
+}
+free(current);
+return (1); 
+}
