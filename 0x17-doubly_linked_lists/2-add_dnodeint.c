ccd782537235fac197785fa8e7381dd11e5e6d95
helmogey <h.elmogey@gmail.com>
2024-01-11 13:01:24 +0200

    c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *add_dnodeint - doubly linked list
+ *@heaf: input
+ *@n: input
+ *Description: doubly linked list node structure
+ *Return: dlistint_t
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+if (new_node == NULL)
+{
+return NULL;
+}
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
+if (*head != NULL)
+{
+(*head)->prev = new_node;
+}
+*head = new_node;
+return (new_node);
+}
