093b6dfa676e46efb921118fa1d6ef5c769b980b
helmogey <h.elmogey@gmail.com>
2024-01-11 13:22:49 +0200

    c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ *add_dnodeint_end - doubly linked list
+ *@head: input
+ *@n: input
+ *Description: doubly linked list node structure
+ *Return: size_t
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+    
+if (new_node == NULL)
+{
+return NULL;
+}
+new_node->n = n;
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return new_node;
+}
+
+dlistint_t *last_node = *head;
+while (last_node->next != NULL)
+{
+last_node = last_node->next;
+}
+
+last_node->next = new_node;
+new_node->prev = last_node;
+
+return new_node;
+}
