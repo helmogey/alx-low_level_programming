4e34444b3a323b81fc2df67a3781473e0e95c714
helmogey <h.elmogey@gmail.com>
2024-01-11 12:31:56 +0200

    c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,18 @@
 #include "lists.h"
 /**
- * print_dlistint - doubly linked list
- * @h: input
- *
- * Description: doubly linked list node structure
- * 
+ *print_dlistint - doubly linked list
+ *@h: input
+ *Description: doubly linked list node structure
+ *Return: size_t
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 size_t node_count = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_count++;
-    }
-
-    return node_count;
-
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+node_count++;
+}
+return node_count;
 }
