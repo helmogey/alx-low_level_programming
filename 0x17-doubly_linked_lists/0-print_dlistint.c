74bd2afca182801ebc3402ce6c2a0266be9cf640
helmogey <h.elmogey@gmail.com>
2024-01-11 12:52:03 +0200

    c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,17 +9,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 int node_count = 0;
 
-if (h == NULL)
-return (node_count);
-while (h->prev != NULL)
-{
-h = h->prev;
-}
 while (h != NULL)
 {
-printf("%d\n", h->n);
-node_count++;
 h = h->next;
+node_count++;
 }
 return (node_count);
 }
