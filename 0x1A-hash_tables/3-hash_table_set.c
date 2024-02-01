7bc60e038e290e671719fef1609c87a8e01c2815
helmogey <h.elmogey@gmail.com>
2024-02-01 20:28:45 +0200

    01A

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - set table
+ * @ht: A pointer.
+ * @key: string.
+ * @value: char.
+ *
+ * Return: int
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+hash_node_t *new;
+char *copy;
+unsigned long int idx, i;
+
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+return (0);
+copy = strdup(value);
+if (copy == NULL)
+return (0);
+idx = key_index((const unsigned char *)key, ht->size);
+for (i = idx; ht->array[i]; i++)
+{
+if (strcmp(ht->array[i]->key, key) == 0)
+{
+free(ht->array[i]->value);
+ht->array[i]->value = copy;
+return (1);
+}
+}
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+{
+free(copy);
+return (0);
+}
+new->key = strdup(key);
+if (new->key == NULL)
+{
+free(new);
+return (0);
+}
+new->value = copy;
+new->next = ht->array[idx];
+ht->array[idx] = new;
+return (1);
+}
