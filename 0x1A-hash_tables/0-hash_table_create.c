671ed774cdda9434ce89ccaf0057956e806068cf
helmogey <h.elmogey@gmail.com>
2024-02-03 10:59:12 +0200

    01A

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	unsigned long int i;
+hash_table_t *table;
+unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
-		return (NULL);
-
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (table->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-
-	return (table);
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+table->size = size;
+table->array = malloc(sizeof(hash_node_t *) * size);
+if (table->array == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+table->array[i] = NULL;
+return (table);
 }
