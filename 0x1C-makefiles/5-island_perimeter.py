3de74e5b4ac8c1025b5a7a16bd2f89beb6eb22f6
helmogey <h.elmogey@gmail.com>
2024-03-20 13:26:41 +0200

    make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """function that returns the perimeter of the island described in grid"""
 
-
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island in the given grid.
