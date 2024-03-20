c28567a11b461b0f9247bb1b8a0e826e82e01b82
helmogey <h.elmogey@gmail.com>
2024-03-20 13:25:50 +0200

    make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,32 +3,28 @@
 
 
 def island_perimeter(grid):
-  """
-  Calculates the perimeter of the island in the given grid.
+    """
+    Calculates the perimeter of the island in the given grid.
 
-  Args:
-      grid: A list of lists of integers representing the grid.
-          0 represents water, 1 represents land.
+    Args:
+        grid: A list of lists of integers representing the grid.
+            0 represents water, 1 represents land.
 
-  Returns:
-      The perimeter of the island as an integer.
-  """
+    Returns:
+        The perimeter of the island as an integer.
+    """
 
-  rows, cols = len(grid), len(grid[0])
-  perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    edges = 0
+    size = 0
 
-  for row in range(rows):
-    for col in range(cols):
-      if grid[row][col] == 1:
-        # Check for land neighbors (up, down, left, right)
-        perimeter += 4  # Assume all sides are water (worst-case scenario)
-        if row > 0 and grid[row - 1][col] == 1:
-          perimeter -= 2  # Not water on top
-        if row < rows - 1 and grid[row + 1][col] == 1:
-          perimeter -= 2  # Not water on bottom
-        if col > 0 and grid[row][col - 1] == 1:
-          perimeter -= 2  # Not water on left
-        if col < cols - 1 and grid[row][col + 1] == 1:
-          perimeter -= 2  # Not water on right
-
-  return perimeter
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    perimeter = size * 4 - edges * 2
+    return perimeter
