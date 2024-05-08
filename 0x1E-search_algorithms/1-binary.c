75cab0809c4792c0dea76978b893f098099e7ad8
helmogey <h.elmogey@gmail.com>
2024-05-08 08:25:35 +0300

    1st

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,13 +10,14 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-int low = 0,high = size - 1, mid, i;
+int low = 0, high = size - 1, mid, i;
 
 if (array == NULL)
 {
 return (-1);
 }
-while (low <= high) {
+while (low <= high)
+{
 mid = low + (high - low) / 2;
 
 printf("Searching in array: ");
@@ -26,7 +27,8 @@ printf("%d, ", array[i]);
 }
 printf("%d\n", array[i]);
 
-if (array[mid] == value) {
+if (array[mid] == value)
+{
 return (mid);
 }
 else if (array[mid] < value)
